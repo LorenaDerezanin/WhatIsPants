eba1480e190526727a57bef309149ac89a2212b6
LorenaDerezanin <10100957+LorenaDerezanin@users.noreply.github.com>
2024-03-24 11:34:36 +0100

    parallelize mask2contour func

--- deepfash/mask_2_contour.py
+++ deepfash/mask_2_contour.py
@@ -1,22 +1,21 @@
 import os
 import cv2
 import numpy as np
+from concurrent.futures import ThreadPoolExecutor
 
 masks_dir = "segm"
+labels_dir = "labels"
 
 # pants are marked with this light gray color in the mask files
 pants_mask_color = np.array([211, 211, 211])
 
-# load labelled mask pngs
-for mask_filename in os.listdir(masks_dir):
+
+def mask2contour(mask_filename: str):
     mask_file_path = os.path.join(masks_dir, mask_filename)
     clothes_mask = cv2.imread(mask_file_path)
-
     # Create a boolean mask where the color of each pixel matches the pants color
     binary_mask = np.all(clothes_mask == pants_mask_color, axis=-1)
 
-    # initialize empty label if no pants found
-    formatted_contour_label = ""
     if np.any(binary_mask):
         # Convert the boolean mask to an integer mask
         # Multiply by 211 to change True values to 211, False values will be 0
@@ -38,13 +37,20 @@ for mask_filename in os.listdir(masks_dir):
 
         # convert contour array to single line string
         formatted_contour_label = "0 " + ' '.join([f'{num:.8f}' for row in normalized_contour for num in row])
+    else:
+        # set empty label if no pants found
+        formatted_contour_label = ""
+        print(f"No pants labelled in {mask_filename}")
 
     contour_filename = mask_filename.replace("_segm.png", ".txt")
-    contour_file_path = os.path.join("labels", contour_filename)
-
+    contour_file_path = os.path.join(labels_dir, contour_filename)
     # write contour to file
     with open(contour_file_path, 'w') as file:
         file.write(formatted_contour_label)
 
 
-
+# load labelled mask pngs
+# Use ThreadPoolExecutor for parallel copying
+with ThreadPoolExecutor(max_workers=10) as executor:
+    for mask_filename in os.listdir(masks_dir):
+        executor.submit(mask2contour, mask_filename)
--- deepfash/subset_images.py
+++ deepfash/subset_images.py
@@ -3,36 +3,31 @@ import random
 import shutil
 from concurrent.futures import ThreadPoolExecutor
 
-WORKDIR = '/data/gpfs-1/users/lode10_c/scratch/yolo_train/distill/'
+WORKDIR = '.'
 
 
 def copy_file(file, destination):
     shutil.copy2(file, destination)
 
 
-def main():
-    source_dir = WORKDIR + 'images_resized'
-    dest_dir = WORKDIR + 'images_subset'
-    num_images = 2000
-    seed = 1234                             # Seed for reproducibility
+segm_source_dir = os.path.join(WORKDIR, '../distill/segm')
+dest_dir = os.path.join(WORKDIR, 'images')
+num_images = 200
+seed = 1234                             # seed for reproducibility
 
-    if not os.path.exists(dest_dir):
-        os.makedirs(dest_dir)
+os.makedirs(dest_dir, exist_ok=True)
 
-    # List all jpg files in the source directory
-    all_files = [os.path.join(source_dir, f) for f in os.listdir(source_dir)]
+# List all jpg files in the source directory
+all_files = [os.path.join(segm_source_dir, f) for f in os.listdir(segm_source_dir)]
 
-    # Set the seed and select random files
-    random.seed(seed)
-    selected_files = random.sample(all_files, num_images)
+# Set the seed and select random files
+random.seed(seed)
+selected_files = random.sample(all_files, num_images)
 
-    # Use ThreadPoolExecutor for parallel copying
-    with ThreadPoolExecutor(max_workers=32) as executor:
-        for file in selected_files:
-            executor.submit(copy_file, file, dest_dir)
+# Use ThreadPoolExecutor for parallel copying
+with ThreadPoolExecutor(max_workers=32) as executor:
+    for file in selected_files:
+        executor.submit(copy_file, file, dest_dir)
 
-
-if __name__ == "__main__":
-    main()
 
 
