e381938aa4d4c4b07ece0ac82f7e02b8733daea4
LorenaDerezanin <10100957+LorenaDerezanin@users.noreply.github.com>
2024-05-05 14:41:25 +0200

    update sbatch configuration
    removing torch kernels cache file in home dir .cache before new train run due to errors otherwise

--- deepfash/sbatch_submit.sh
+++ deepfash/sbatch_submit.sh
@@ -3,8 +3,8 @@
 #SBATCH --job-name=train
 #SBATCH --gres=gpu:tesla:2
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=20
-#SBATCH --mem=128GB
+#SBATCH --cpus-per-task=20        # with 16 and fewer we get an error in os.sched_setaffinity(0, range(NUM_THREADS))
+#SBATCH --mem=32GB
 #SBATCH --time=2-00:00:00              # Time limit d-hrs:min:sec
 #SBATCH --partition=gpu
 
@@ -19,10 +19,17 @@ date
 
 export TMPDIR=/fast/users/lode10_c/scratch/yolo_train/tmp
 
-source /data/gpfs-1/users/lode10_c/.bashrc
+source "$HOME"/.bashrc
 conda activate /data/gpfs-1/users/lode10_c/scratch/yolo_train/conda/whatispants
 
-export CUDA_LAUNCH_BLOCKING=1
+# Clear torch kernels cache to prevent errors like
+#   File "/data/gpfs-1/users/lode10_c/scratch/yolo_train/conda/whatispants/lib/python3.12/site-packages/ultralytics/utils/loss.py", line 408, in calculate_segmentation_loss
+#    marea = xyxy2xywh(target_bboxes_normalized)[..., 2:].prod(2)
+#            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# RuntimeError: CUDA driver error: invalid argument
+# See also https://discuss.pytorch.org/t/torch-prod-produces-runtimeerror-cuda-driver-error-invalid-argument/179054/20
+rm -rf "$HOME"/.cache/torch/kernels
+
 python /data/gpfs-1/users/lode10_c/scratch/yolo_train/deepfash/train.py --epochs $EPOCHS
 
 echo "Yolo train job done"
