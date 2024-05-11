69fa7be3524f6717db4e783e7f8cc07d48af01e4
LorenaDerezanin <10100957+LorenaDerezanin@users.noreply.github.com>
2024-05-11 16:50:26 +0200

    Update lvis_yolo_train.py with logging and such

--- lvis_yolo_train.py
+++ lvis_yolo_train.py
@@ -1,7 +1,22 @@
+import argparse
+
+import torch
 from ultralytics import YOLO
 
-# Load a model
-model = YOLO('yolov8m.pt')  # load a pretrained model (recommended for training)
+# Set up argument parser
+parser = argparse.ArgumentParser(description='Train YOLO model on custom dataset.')
+parser.add_argument('--epochs', type=int, default=5, help='Number of epochs to train the model.')
+args = parser.parse_args()
 
-# Train the model
-results = model.train(data='lvis.yaml', epochs=50, imgsz=640)
+# Check if GPU is working or not
+torch_version = torch.__version__
+device_name = torch.cuda.get_device_properties(0).name if torch.cuda.is_available() else 'CPU'
+print(
+    f"Using torch {torch_version} ({device_name})")
+
+
+# train custom model
+print(f"Training on lvis dataset on {args.epochs} epochs")
+model = YOLO("yolov8m-seg.pt")
+model.train(data='lvis.yaml', epochs=args.epochs, imgsz=640)
+print("done")
