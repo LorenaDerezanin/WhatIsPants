aa49e804e0d8ea04be1c18ee6a1c04d0dada02cb
LorenaDerezanin <10100957+LorenaDerezanin@users.noreply.github.com>
2024-05-12 18:20:10 +0200

    Use new combined lvis & deepfash datasets

--- lvis_yolo_train.py
+++ lvis_yolo_train.py
@@ -6,6 +6,8 @@ from ultralytics import YOLO
 # Set up argument parser
 parser = argparse.ArgumentParser(description='Train YOLO model on custom dataset.')
 parser.add_argument('--epochs', type=int, default=5, help='Number of epochs to train the model.')
+parser.add_argument('--size', default='m', help='Yolo segmentation model size, one of (n, s, m, l, x)')
+parser.add_argument('--yaml', default='lvis', help='Name of the yaml file to use (without extension)')
 args = parser.parse_args()
 
 # Check if GPU is working or not
@@ -16,7 +18,9 @@ print(
 
 
 # train custom model
-print(f"Training on lvis dataset on {args.epochs} epochs")
-model = YOLO("yolov8m-seg.pt")
-model.train(data='lvis.yaml', epochs=args.epochs, imgsz=640)
+model_name = f"yolov8{args.size}-seg.pt"
+print(f"Training on lvis dataset on {args.epochs} epochs based on {model_name}")
+model = YOLO(model_name)
+name = f"{args.yaml}_{args.size}_{args.epochs}_train"
+model.train(data=f"{args.yaml}.yaml", epochs=args.epochs, imgsz=640, name=name)
 print("done")
